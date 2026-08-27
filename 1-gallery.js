173daaec08d548eb3c5cfed5708953ec5be2bec4
YuraKirpichnikov <Georgestallker@gmail.com>
2026-08-27 15:20:19 +0000

    Deploying to gh-pages from @ YuraKirpichnikov/goit-js-hw-09@3cf5153435b7293bc1dddcabbb26ba34d7fa5eba 🚀

---
+++ 1-gallery.js
@@ -0,0 +1,12 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{S as p}from"./assets/vendor-CgTBfC_f.js";const o=document.querySelector(".gallery"),e=[{preview:"https://picsum.photos/id/10/300/200",original:"https://picsum.photos/id/10/1200/800",description:"Lake and forest"},{preview:"https://picsum.photos/id/20/300/200",original:"https://picsum.photos/id/20/1200/800",description:"Laptop on desk"},{preview:"https://picsum.photos/id/30/300/200",original:"https://picsum.photos/id/30/1200/800",description:"City street"},{preview:"https://picsum.photos/id/40/300/200",original:"https://picsum.photos/id/40/1200/800",description:"Mountain view"},{preview:"https://picsum.photos/id/50/300/200",original:"https://picsum.photos/id/50/1200/800",description:"Snowy hill"},{preview:"https://picsum.photos/id/60/300/200",original:"https://picsum.photos/id/60/1200/800",description:"Sea coast"}],a=e.map(({preview:i,original:t,description:s})=>`
+      <li class="gallery-item">
+        <a class="gallery-link" href="${t}">
+          <img
+            class="gallery-image"
+            src="${i}"
+            alt="${s}"
+          />
+        </a>
+      </li>
+    `).join("");o.insertAdjacentHTML("beforeend",a);new p(".gallery-link",{captionsData:"alt",captionDelay:250});
+//# sourceMappingURL=1-gallery.js.map
